a364942b10824b483cc8c833349040cc48803cfa
albeiro036 <albeiro036@gmail.com>
2023-04-07 13:17:28 +0000

    wait response

--- main.js
+++ main.js
@@ -1,57 +1,56 @@
-var calNow = document.querySelector("#calcValues");
+/* inputs values */
 var salesPrice = document.querySelector("#sales-price");
 var downPaymnt = document.querySelector("#down-payment");
 var intereRate = document.querySelector("#int-rate");
+/* calculate button */
+var calNow = document.querySelector("#calcValues");
+/* constant values */
 var paymentPerYear = 12;
-var loanAmount;
-
-//results
+var rateOne = 0.5;
+/* result inputs */
 var resInterest = document.getElementById('res-interest');
 var resInsurance = document.getElementById('res-insurance');
 var resMortgage = document.getElementById('res-mortgage');
 var resTPay = document.getElementById('res-totalPay');
-
-function resLoanAmount(salesPrice, downPayment) {
-    var intDownPayment = downPayment / 100;
-    var result = salesPrice * intDownPayment;
-    result = salesPrice - result;
-    return result;
+function calLoanAmount(salesPrice, downPayment) {
+    return salesPrice - (salesPrice * (downPayment / 100));
 }
-
-function calcInsurance(salesPrice) {
-    var intBornedInsu = (0.5 / 100);
-    return (salesPrice * intBornedInsu) / paymentPerYear;
+function calcInsurance(salesPrice, payPerYear, rateOnep) {
+    var intBornedInsurance = (rateOnep / 100);
+    return (salesPrice * intBornedInsurance) / payPerYear;
 }
-
-function calcProperyTaxes(salesPrice) {
-    var intBornedTaxes = (1.04 / 100);
-    return salesPrice * intBornedTaxes / paymentPerYear;
+function calcProperyTaxes(salesPrice, payPerYear) {
+    var intBurnedTaxes = (1.04 / 100);
+    return salesPrice * intBurnedTaxes / payPerYear;
 }
-
-function calcMortgageInsurance(loanAmnt) {
-    var intBornedMortgage = (0.55 / 100);
-    return loanAmnt * intBornedMortgage / paymentPerYear;
+function calcMortgageInsurance(loanAmnt, payPerYear) {
+    var intBurnedMortgage = (0.55 / 100); //@to-do comprobar que sea igual a rate1st
+    return loanAmnt * intBurnedMortgage / payPerYear;
 }
-
-function PAGO(amount, interest, PayPerYear, numPerYear) {
-    var res = (amount * (interest / numPerYear)) / (1 - (1 + (interest / numPerYear)) ** (-(PayPerYear * numPerYear)));
-    return res;
+function calInterestRate(amount, interest, payPerDays, numPerYear) {
+    var numerator = (amount * (interest / numPerYear));
+    var denominator = (1 - Math.pow((1 + (interest / numPerYear)), (-(payPerDays * numPerYear))));
+    return numerator / denominator;
 }
-
-calNow.addEventListener("click", function () {
+function resTotalPayments(intRate, insurance, propTaxes, mortgageInsu) {
+    var result = (intRate + insurance + propTaxes + mortgageInsu);
+    return result.toFixed(0);
+}
+function formatResult(resNumber) {
+    var withOutDec = resNumber.toFixed(0);
+    return new Intl.NumberFormat().format(+withOutDec);
+}
+function writeResult() {
     var intBurnedResult = 6.5 / 100;
-    loanAmount = resLoanAmount(+salesPrice.value, +downPaymnt.value);
-    console.log('Loan amount:', loanAmount);
-    console.log('sales price:', calcInsurance(+salesPrice.value).toFixed(2));
-    console.log('taxes:', calcProperyTaxes(+salesPrice.value).toFixed(2));
-    console.log('Mortgage:', calcMortgageInsurance(+loanAmount).toFixed(2));
-    console.log('Interest rate:', PAGO(+loanAmount, +intBurnedResult, 30, +paymentPerYear).toFixed(2));
-    var result = calcProperyTaxes(+salesPrice.value) +
-        PAGO(+loanAmount, +intBurnedResult, 30, +paymentPerYear) +
-        calcInsurance(+salesPrice.value) +
-        calcMortgageInsurance(loanAmount);
-    resInterest.innerHTML = "$ " + PAGO(+loanAmount, +intBurnedResult, 30, +paymentPerYear).toFixed(2);
-    resInsurance.innerHTML = "$ " + calcInsurance(+salesPrice.value).toFixed(0);
-    resMortgage.innerHTML = "$ " + calcMortgageInsurance(loanAmount).toFixed(2);
-    resTPay.innerHTML = "$ " + result.toFixed(0);
-});
+    var loanAmount = calLoanAmount(+salesPrice.value, +downPaymnt.value);
+    var resIntRate = calInterestRate(loanAmount, intBurnedResult, 30, paymentPerYear);
+    var resInsurancep = calcInsurance(+salesPrice.value, paymentPerYear, rateOne);
+    var calcPropTaxes = calcProperyTaxes(+salesPrice.value, paymentPerYear);
+    var mortgageInsurance = calcMortgageInsurance(loanAmount, paymentPerYear);
+    var totalPayment = resTotalPayments(resIntRate, resInsurancep, calcPropTaxes, mortgageInsurance);
+    resInterest.innerHTML = formatResult(resIntRate);
+    resInsurance.innerHTML = formatResult(resInsurancep);
+    resMortgage.innerHTML = formatResult(mortgageInsurance);
+    resTPay.innerHTML = formatResult(+totalPayment);
+}
+calNow.addEventListener("click", writeResult);
--- main.ts
+++ main.ts
@@ -1,52 +1,67 @@
-import Math from 'Math';
-var calNow = document.querySelector("#calcValues");
-var paymentPerYear = 12;
-var salesPrice;
-var downPaymnt;
-var intereRate;
-var loanAmount
+/* inputs values */
+const salesPrice = document.querySelector<HTMLInputElement>("#sales-price");
+const downPaymnt = document.querySelector<HTMLInputElement>("#down-payment");
+const intereRate = document.querySelector<HTMLInputElement>("#int-rate");
+/* calculate button */
+const calNow = document.querySelector<HTMLButtonElement>("#calcValues");
+/* constant values */
+const paymentPerYear = 12;
+const rateOne = 0.5;
 
-function resLoanAmount(salesPrice: number, downPayment: number) {
-    var intDownPayment = downPayment / 100;
-    var result = salesPrice * intDownPayment;
-    result = salesPrice - result;
-    return result;
+/* result inputs */
+const resInterest = document.getElementById('res-interest') as HTMLHeadingElement;
+const resInsurance = document.getElementById('res-insurance') as HTMLHeadingElement;
+const resMortgage = document.getElementById('res-mortgage') as HTMLHeadingElement;
+const resTPay = document.getElementById('res-totalPay') as HTMLHeadingElement;
+
+
+function calLoanAmount(salesPrice: number, downPayment: number): number {
+    return salesPrice - (salesPrice * (downPayment / 100));
 }
 
-function calcInsurance(salesPrice) {
-    var intBornedInsu = (25 / 100);
-    return salesPrice * intBornedInsu / paymentPerYear;
+function calcInsurance(salesPrice: number, payPerYear: number, rateOnep: number): number {
+    const intBornedInsurance = (rateOnep / 100);
+    return (salesPrice * intBornedInsurance) / payPerYear;
 }
 
-function calcProperyTaxes(salesPrice) {
-    var intBornedTaxes = (1.04 / 100);
-    return salesPrice * intBornedTaxes / paymentPerYear;
+function calcProperyTaxes(salesPrice: number, payPerYear: number): number {
+    const intBurnedTaxes = (1.04 / 100);
+    return salesPrice * intBurnedTaxes / payPerYear;
 }
 
-function calcMortgageInsurance(loanAmnt) {
-    var intBornedMortgage = (0.55 / 100);
-    return loanAmnt * intBornedMortgage / paymentPerYear;
+function calcMortgageInsurance(loanAmnt: number, payPerYear: number): number {
+    const intBurnedMortgage = (0.55 / 100);//@to-do comprobar que sea igual a rate1st
+    return loanAmnt * intBurnedMortgage / payPerYear;
 }
 
-function PAGO(amount, interest, PayPerYear, numPerYear) {
-  var res = (amount * (interest / numPerYear)) / (1 - (1 + (interest / numPerYear)) ** (-(PayPerYear * numPerYear)));
-  return res;
+function calInterestRate(amount: number, interest: number, payPerDays: number, numPerYear: number) {
+    const numerator = (amount * (interest / numPerYear));
+    const denominator = (1 - (1 + (interest / numPerYear)) ** (-(payPerDays * numPerYear)));
+    return numerator / denominator;
 }
 
-calNow.addEventListener("click", function () {
-    var intBurnedResult = 6.88 / 100;
-    salesPrice = document.querySelector<HTMLInputElement>("#sales-price");
-    downPaymnt = document.querySelector<HTMLInputElement>("#down-payment");
-    intereRate = document.querySelector<HTMLInputElement>("#int-rate");
-    loanAmount = resLoanAmount(+salesPrice!.value, +downPaymnt!.value);
-    console.log(PAGO(+loanAmount, +intBurnedResult, 30, +paymentPerYear));
-});
-
-
-function initialValues() {
-    document.querySelector("#sales-price").value = '350000'
-    document.querySelector("#down-payment").value = 20;
-    document.querySelector("#int-rate").value = 6.88
+function resTotalPayments(intRate: number, insurance: number, propTaxes: number, mortgageInsu: number) {
+    const result = (intRate + insurance + propTaxes + mortgageInsu);
+    return result.toFixed(0);
 }
 
-initialValues();
+function formatResult(resNumber: number) {
+    const withOutDec = resNumber.toFixed(0);
+    return new Intl.NumberFormat().format(+withOutDec);
+}
+
+function writeResult() {
+    const intBurnedResult = 6.5 / 100;
+    const loanAmount = calLoanAmount(+salesPrice!.value, +downPaymnt!.value);
+    const resIntRate = calInterestRate(loanAmount, intBurnedResult, 30, paymentPerYear);
+    const resInsurancep = calcInsurance(+salesPrice!.value, paymentPerYear, rateOne);
+    const calcPropTaxes = calcProperyTaxes(+salesPrice!.value, paymentPerYear);
+    const mortgageInsurance = calcMortgageInsurance(loanAmount, paymentPerYear);
+    const totalPayment = resTotalPayments(resIntRate, resInsurancep, calcPropTaxes, mortgageInsurance);
+    resInterest.innerHTML = formatResult(resIntRate);
+    resInsurance.innerHTML = formatResult(resInsurancep);
+    resMortgage.innerHTML = formatResult(mortgageInsurance);
+    resTPay.innerHTML = formatResult(+totalPayment);
+}
+
+calNow!.addEventListener("click", writeResult);
